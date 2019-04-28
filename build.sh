f30edc2cd5d21ef2f6802b9f084cd82cb96c1cb6
Ant Kutschera <ant.kutschera@gmail.com>
2019-04-28 16:38:50 +0200

    fixed kibana in dev

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ docker build -f DockerfileElasticsearch -t maxant/elasticsearch .
 
 docker build -f DockerfileNeo4j -t maxant/neo4j .
 
+docker build -f DockerfileKibana -t maxant/kibana .
+
 ps ax | grep web-microbundle | grep -v grep | awk '{print $1}' | xargs kill
 
 ps ax | grep tasks-microbundle | grep -v grep | awk '{print $1}' | xargs kill
