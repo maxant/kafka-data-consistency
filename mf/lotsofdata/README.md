34d192198ef310464f97f935e67ad6d7bafee5cc
Ant Kutschera <1108507+maxant@users.noreply.github.com>
2021-01-21 20:23:48 +0100

    lotsofdata tests

--- mf/lotsofdata/README.md
+++ mf/lotsofdata/README.md
@@ -1,21 +1,87 @@
 
+    # move data folder to usb
+    sudo cp -R -p /var/lib/mysql/* /media/usb2/mysql
+
     # create user in linux too, using same password, so he can access the import file
     forget this: sudo adduser mfcontracts
     ditto: sudo chown mfcontracts:mfcontracts import-contracts.csv
 
-    mysql -u root -p mfcontracts
+    # login as root, so we can do load data
+    su
+    mysql mfcontracts
 
     -- not used: grant file on *.* to mfcontracts@localhost identified by 'secret';
 
+https://dba.stackexchange.com/questions/98384/need-to-make-mysqls-load-data-local-infile-load-large-data-faster
+https://derwiki.tumblr.com/post/24490758395/loading-half-a-billion-rows-into-mysql
 
-LOAD DATA LOCAL INFILE 'import-contracts.csv' INTO TABLE mfcontracts.T_CONTRACTS
-FIELDS TERMINATED by ','
-ENCLOSED BY '\"'
-LINES TERMINATED by '\n'
-IGNORE 1 ROWS
-(ID, STARTTIME, ENDTIME, STATE, SYNC_TIMESTAMP, CREATED_AT, CREATED_BY, OFFERED_AT, OFFERED_BY, ACCEPTED_AT, ACCEPTED_BY, APPROVED_AT, APPROVED_BY)
-;
+    SET FOREIGN_KEY_CHECKS = 0;
+    SET UNIQUE_CHECKS = 0;
+    SET SESSION tx_isolation='READ-UNCOMMITTED';
+    SET sql_log_bin = 0;
+    ALTER TABLE T_CONTRACTS DISABLE KEYS;
+    LOCK TABLES T_CONTRACTS WRITE;
 
+    SET GLOBAL innodb_buffer_pool_size=268435456; -- 256MB should be similar to input file size
+    -- dont work: SET GLOBAL innodb_write_io_threads=16; -- add more concurrency - does that work on usb3.0?
+    -- dont work: SET GLOBAL innodb_log_buffer_size=268435456; -- 256MB should be similar to input file size
 
-select * from mfcontracts.T_CONTRACTS;
-delete from mfcontracts.T_CONTRACTS;
+afterwards: 
+
+    SET UNIQUE_CHECKS = 1;
+    SET FOREIGN_KEY_CHECKS = 1;
+    SET SESSION tx_isolation='READ-REPEATABLE';
+    UNLOCK T_CONTRACTS;
+    ALTER TABLE verification ENABLE KEYS;
+
+    SET GLOBAL innodb_buffer_pool_size=134217728; -- 128MB
+    -- dont work: SET GLOBAL innodb_write_io_threads=4;
+    -- dont work: SET GLOBAL innodb_log_buffer_size=8388608; -- 8MB
+
+    mvn compile exec:java -Dexec.mainClass="Generator"
+
+    delete from mfcontracts.T_CONTRACTS;
+    
+    select * from mfcontracts.T_CONTRACTS;
+
+-- doesnt seem necessary, as "local" in the next statement seems to do this anyway:
+-- START TRANSACTION; -- so that each insert is not it's own tx
+
+    LOAD DATA LOCAL INFILE 'import-contracts2.csv' INTO TABLE mfcontracts.T_CONTRACTS
+    FIELDS TERMINATED by ','
+    ENCLOSED BY '\"'
+    LINES TERMINATED by '\n'
+    IGNORE 1 ROWS
+    (ID, STARTTIME, ENDTIME, STATE, SYNC_TIMESTAMP, CREATED_AT, CREATED_BY, OFFERED_AT, OFFERED_BY, ACCEPTED_AT, ACCEPTED_BY, APPROVED_AT, APPROVED_BY)
+    ;
+
+-- see above:
+-- COMMIT;
+
+    SELECT table_rows FROM information_schema.tables WHERE table_name = 'T_CONTRACTS';
+
+1'000'000 contract records => 262 MB
+18 mins load time
+USB 180GB partition over USB3.0
+
+    sudo du -h --max-depth=1
+
+      1.3M	./mysql
+      281M	./mfcontracts
+      8.0K	./performance_schema
+      466M	. (total, 281M in mfcontracts, the rest here:
+
+      48M Jan 21 08:41 ib_logfile0
+      48M Jan 21 08:41 ib_logfile1
+      76M Jan 21 08:41 ibdata1
+      12M Jan 20 21:40 ibtmp1
+
+mfcontracts folder:
+
+    3.3K Jan 20 21:42 T_COMPONENTS.frm
+    144K Jan 20 21:42 T_COMPONENTS.ibd
+    2.6K Jan 20 21:42 T_CONDITIONS.frm
+    144K Jan 20 21:42 T_CONDITIONS.ibd
+    2.4K Jan 20 21:42 T_CONTRACTS.frm
+    280M Jan 21 08:41 T_CONTRACTS.ibd
+
--- mf/lotsofdata/src/main/kotlin/Generator.kt
+++ mf/lotsofdata/src/main/kotlin/Generator.kt
@@ -1,8 +1,7 @@
-import java.nio.file.Files
-import java.io.FileWriter
-
 import java.io.BufferedWriter
 import java.io.File
+import java.io.FileWriter
+import java.sql.DriverManager
 import java.util.*
 
 
@@ -10,21 +9,58 @@ class Generator {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val file = File("./mf/lotsofdata/import-contracts2.csv")
-            if(file.exists()) file.delete()
-            val out = BufferedWriter(FileWriter(file), 32768)
-            var start: Long = System.currentTimeMillis()
-            var bytes = 0L
-            for(i in 1..1000) {
-                val text = """"${UUID.randomUUID()}","2021-12-10 00:00:00.000","2023-11-30 00:00:00.000","RUNNING","1609610495490","2021-01-02 18:01:34.681","john.smith","2021-01-02 18:01:36.889","john.smith","2021-01-02 18:01:37.516","john.smith ","2021-01-02 18:01:38.443","jane.smith""""
-                out.write(text)
-                out.newLine()
-                bytes += text.length
+            for(j in 1..20) {
+                println("ITERATION $j")
+                val file = File("./mf/lotsofdata/import-contracts2.csv")
+                if(file.exists()) file.delete()
+                val out = BufferedWriter(FileWriter(file), 32768)
+                var start: Long = System.currentTimeMillis()
+                var bytes = 0L
+                for(i in 1..1000) {
+                    val text = """"${UUID.randomUUID()}","2021-12-10 00:00:00.000","2023-11-30 00:00:00.000","RUNNING","1609610495490","2021-01-02 18:01:34.681","john.smith","2021-01-02 18:01:36.889","john.smith","2021-01-02 18:01:37.516","john.smith ","2021-01-02 18:01:38.443","jane.smith""""
+                    out.write(text)
+                    out.newLine()
+                    bytes += text.length
+                }
+                out.close()
+                val time = System.currentTimeMillis() - start
+                System.out.println("Wrote ${bytes/1024}KB in $time ms")
+                System.out.println("${(bytes / 1024.0 / 1024L / (time / 1000.0))} MB/s.")
+
+                Class.forName("com.mysql.cj.jdbc.Driver")
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/mfcontracts","root", System.getProperty("password")).use { c ->
+                    c.prepareStatement("""
+    SET FOREIGN_KEY_CHECKS = 0;
+    SET UNIQUE_CHECKS = 0;
+    SET SESSION tx_isolation='READ-UNCOMMITTED';
+    SET sql_log_bin = 0;
+    ALTER TABLE T_CONTRACTS DISABLE KEYS;
+    LOCK TABLES T_CONTRACTS WRITE;
+    SET GLOBAL innodb_buffer_pool_size=268435456; -- 256MB should be similar to input file size
+
+    LOAD DATA LOCAL INFILE 'import-contracts2.csv' INTO TABLE mfcontracts.T_CONTRACTS2
+    FIELDS TERMINATED by ','
+    ENCLOSED BY '\"'
+    LINES TERMINATED by '\n'
+    IGNORE 1 ROWS
+    (ID, STARTTIME, ENDTIME, STATE, SYNC_TIMESTAMP, CREATED_AT, CREATED_BY, OFFERED_AT, OFFERED_BY, ACCEPTED_AT, ACCEPTED_BY, APPROVED_AT, APPROVED_BY)
+    ;
+                    """.trimIndent()).use { ps ->
+                        println("updated ${ps.executeUpdate()} rows")
+                    }
+                    c.prepareStatement("""
+    SET UNIQUE_CHECKS = 1;
+    SET FOREIGN_KEY_CHECKS = 1;
+    SET SESSION tx_isolation='READ-REPEATABLE';
+    UNLOCK T_CONTRACTS;
+    ALTER TABLE verification ENABLE KEYS;
+
+    SET GLOBAL innodb_buffer_pool_size=134217728; -- 128MB
+                    """.trimIndent()).use { ps ->
+                        println("updated ${ps.executeUpdate()} rows")
+                    }
+                }
             }
-            out.close()
-            val time = System.currentTimeMillis() - start
-            System.out.println("Wrote ${bytes/1024}KB in $time ms")
-            System.out.println("${(bytes / 1024.0 / 1024L / (time / 1000.0))} MB/s.")
         }
     }
 }
