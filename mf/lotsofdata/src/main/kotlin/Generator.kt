160565fc4a3c5cf39ff06e4d914c0b532346b57c
Ant Kutschera <1108507+maxant@users.noreply.github.com>
2021-01-21 21:25:00 +0100

    lots of data

--- mf/lotsofdata/src/main/kotlin/Generator.kt
+++ mf/lotsofdata/src/main/kotlin/Generator.kt
@@ -1,7 +1,9 @@
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
+import java.sql.Connection
 import java.sql.DriverManager
+import java.text.DecimalFormat
 import java.util.*
 
 
@@ -9,56 +11,62 @@ class Generator {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            for(j in 1..20) {
+            Class.forName("com.mysql.cj.jdbc.Driver")
+            for(j in 1..30) {
                 println("ITERATION $j")
+
                 val file = File("./mf/lotsofdata/import-contracts2.csv")
                 if(file.exists()) file.delete()
-                val out = BufferedWriter(FileWriter(file), 32768)
+                val out = BufferedWriter(FileWriter(file), 256*1024)
                 var start: Long = System.currentTimeMillis()
                 var bytes = 0L
-                for(i in 1..1000) {
+                for(i in 1..10_000) {
                     val text = """"${UUID.randomUUID()}","2021-12-10 00:00:00.000","2023-11-30 00:00:00.000","RUNNING","1609610495490","2021-01-02 18:01:34.681","john.smith","2021-01-02 18:01:36.889","john.smith","2021-01-02 18:01:37.516","john.smith ","2021-01-02 18:01:38.443","jane.smith""""
                     out.write(text)
                     out.newLine()
                     bytes += text.length
                 }
                 out.close()
-                val time = System.currentTimeMillis() - start
-                System.out.println("Wrote ${bytes/1024}KB in $time ms")
-                System.out.println("${(bytes / 1024.0 / 1024L / (time / 1000.0))} MB/s.")
+                var time = System.currentTimeMillis() - start
+                System.out.println("Wrote ${bytes/1024}KB in $time ms => ${DecimalFormat("#.00").format(bytes / 1024.0 / 1024L / (time / 1000.0))} MB/s.")
 
-                Class.forName("com.mysql.cj.jdbc.Driver")
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/mfcontracts","root", System.getProperty("password")).use { c ->
+                DriverManager.getConnection("jdbc:mysql://retropie:3306/mfcontracts?allowLoadLocalInfile=true","mfcontracts", "secret").use { c ->
+                    start = System.currentTimeMillis()
+                    c.autoCommit = false
+                    c.transactionIsolation = Connection.TRANSACTION_READ_UNCOMMITTED
+                    c.prepareStatement("SET FOREIGN_KEY_CHECKS = 0").use { it.executeUpdate() }
+                    c.prepareStatement("SET UNIQUE_CHECKS = 0").use { it.executeUpdate() }
+                    c.prepareStatement("SET sql_log_bin = 0").use { it.executeUpdate() }
+                    c.prepareStatement("SET GLOBAL innodb_buffer_pool_size=268435456").use { it.executeUpdate() }
+                    c.prepareStatement("ALTER TABLE T_CONTRACTS2 DISABLE KEYS").use { it.executeUpdate() }
+                    time = System.currentTimeMillis() - start
+                    System.out.println("setup done in $time ms")
+
+                    start = System.currentTimeMillis()
+                    c.prepareStatement("LOCK TABLES T_CONTRACTS2 WRITE").use { it.executeUpdate() }
                     c.prepareStatement("""
-    SET FOREIGN_KEY_CHECKS = 0;
-    SET UNIQUE_CHECKS = 0;
-    SET SESSION tx_isolation='READ-UNCOMMITTED';
-    SET sql_log_bin = 0;
-    ALTER TABLE T_CONTRACTS DISABLE KEYS;
-    LOCK TABLES T_CONTRACTS WRITE;
-    SET GLOBAL innodb_buffer_pool_size=268435456; -- 256MB should be similar to input file size
+                        LOAD DATA LOCAL INFILE '/w/kafka-data-consistency/mf/lotsofdata/import-contracts2.csv' INTO TABLE mfcontracts.T_CONTRACTS2
+                        FIELDS TERMINATED by ','
+                        ENCLOSED BY '\"'
+                        LINES TERMINATED by '\n'
+                        (ID, STARTTIME, ENDTIME, STATE, SYNC_TIMESTAMP, CREATED_AT, CREATED_BY, OFFERED_AT, OFFERED_BY, ACCEPTED_AT, ACCEPTED_BY, APPROVED_AT, APPROVED_BY)
+                    """.trimIndent()).use { println("load data: ${it.executeUpdate()}") }
+                    c.prepareStatement("UNLOCK TABLES").use { it.executeUpdate() }
+                    time = System.currentTimeMillis() - start
+                    System.out.println("loaded in $time ms")
 
-    LOAD DATA LOCAL INFILE 'import-contracts2.csv' INTO TABLE mfcontracts.T_CONTRACTS2
-    FIELDS TERMINATED by ','
-    ENCLOSED BY '\"'
-    LINES TERMINATED by '\n'
-    IGNORE 1 ROWS
-    (ID, STARTTIME, ENDTIME, STATE, SYNC_TIMESTAMP, CREATED_AT, CREATED_BY, OFFERED_AT, OFFERED_BY, ACCEPTED_AT, ACCEPTED_BY, APPROVED_AT, APPROVED_BY)
-    ;
-                    """.trimIndent()).use { ps ->
-                        println("updated ${ps.executeUpdate()} rows")
-                    }
-                    c.prepareStatement("""
-    SET UNIQUE_CHECKS = 1;
-    SET FOREIGN_KEY_CHECKS = 1;
-    SET SESSION tx_isolation='READ-REPEATABLE';
-    UNLOCK T_CONTRACTS;
-    ALTER TABLE verification ENABLE KEYS;
+                    start = System.currentTimeMillis()
+                    c.commit()
+                    time = System.currentTimeMillis() - start
+                    System.out.println("committed in $time ms")
 
-    SET GLOBAL innodb_buffer_pool_size=134217728; -- 128MB
-                    """.trimIndent()).use { ps ->
-                        println("updated ${ps.executeUpdate()} rows")
-                    }
+                    start = System.currentTimeMillis()
+                    c.prepareStatement("SET UNIQUE_CHECKS = 1").use { it.executeUpdate() }
+                    c.prepareStatement("SET FOREIGN_KEY_CHECKS = 1").use { it.executeUpdate() }
+                    c.prepareStatement("ALTER TABLE T_CONTRACTS2 ENABLE KEYS").use { it.executeUpdate() }
+                    c.prepareStatement("SET GLOBAL innodb_buffer_pool_size=134217728").use { it.executeUpdate() }
+                    time = System.currentTimeMillis() - start
+                    System.out.println("tore down in $time ms")
                 }
             }
         }
